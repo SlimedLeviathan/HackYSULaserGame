ce62685fa0c63e7c20920d51a8fd9445e157c018
SlimedLeviathan <cantonsteven6@gmail.com>
2023-04-15 15:16:20 -0400

    playerlooks at level lol

--- gamecomp.py
+++ gamecomp.py
@@ -2,13 +2,29 @@ import pygame
 pg = pygame 
 from pygame.locals import *
 
-window = pg.display.set_mode((600, 600))
+from neededCode import load, Level, tileList
+
+import tkinter as tk
+root = tk.Tk()
+
+width = root.winfo_screenwidth()
+height = root.winfo_screenheight()
+
+root.quit()
+
+xPadding = 50
+yPadding = 100
+
+gameWidth = width - xPadding * 2
+gameHeight = height - yPadding * 2
+
+window = pg.display.set_mode()
 Player_image = pg.image.load(r'Player_image.png')
 
 class Player:
-    def __init__(self):
-        self.x = 100
-        self.y = 100
+    def __init__(self, x = 100, y = 100):
+        self.x = x
+        self.y = y
         self.frame = 0
         self.health = 10
         self.isjummping = False
@@ -18,21 +34,52 @@ class Player:
         if self.isjummping is False:
             self.isjummping = True
 
-    def gravity(self, object):  
-        self.falling = object.playerInteraction()
+    def gravity(self): 
+        playerBottomLeft = [playerRect.bottomleft[0] - levelWidthPadding,playerRect.bottomleft[1] + 1]
+        playerBottomRight = [playerRect.bottomright[0] - levelWidthPadding,playerRect.bottomright[1] + 1]
+    
+        leftBlockCoords = [int(playerBottomLeft[0] / blockLength), int(playerBottomLeft[1] / blockLength)]
+        rightBlockCoords = [int(playerBottomRight[0] / blockLength), int(playerBottomRight[1] / blockLength)] 
+
+        self.falling = tileList[level.tileList[leftBlockCoords[0]][leftBlockCoords[1]].object].playerInteraction() or tileList[level.tileList[rightBlockCoords[0]][rightBlockCoords[1]].object].playerInteraction()
+
         if self.falling == True and self.isjummping == False:
-            player.y -= 1
+            player.y += 1
         elif self.falling == False:
             pass
-
-
-player = Player()
-
 
 velocity = .5
 
 run = True
-while run :
+
+level = Level('First Level',16,16)
+
+load(1, level)
+
+blockLength = min(height / len(level.tileList[0]), width / len(level.tileList))
+
+levelWidth = blockLength * len(level.tileList)
+levelHeight = blockLength * len(level.tileList[0])
+
+levelWidthPadding = (gameWidth - levelWidth) / 2
+levelHeightPadding = (gameHeight - levelHeight) / 2
+
+done = False
+
+for xNum in range(len(level.tileList)):
+    if done == True:
+        break
+
+    for yNum in range(len(level.tileList[xNum])):
+        if level.tileList[xNum][yNum].object == 9:
+            done = True
+            x = xNum
+            y = yNum
+            break
+
+player = Player(xPadding + levelWidthPadding + (x * (levelWidth / len(level.tileList))), yPadding + levelHeightPadding + (y * (levelHeight / len(level.tileList[0]))))
+
+while run == True:
     
     for event in pg.event.get():
         if event.type ==pg.QUIT:
@@ -41,18 +88,33 @@ while run :
 
     window.fill([0,0,0])
     #window.fill(225, 225, 225)
-    window.blit(Player_image, (player.x, player.y))
 
     keysPressed = pg.key.get_pressed()
 
     jumpingCount = 2
+
+    for xNum in range(len(level.tileList)):
+        for yNum in range(len(level.tileList[0])):
+            pg.draw.rect(window, tileList[level.tileList[xNum][yNum].object].color, [xPadding + levelWidthPadding + (xNum * (levelWidth / len(level.tileList))), yPadding + levelHeightPadding + (yNum * (levelHeight / len(level.tileList[0]))), blockLength, blockLength])
     
+    playerSurface = pg.Surface([blockLength,blockLength])
+
+    playerSurface.blit(pg.transform.scale(Player_image, [blockLength, blockLength]),[0,0])
+    playerRect = window.blit(playerSurface, (player.x, player.y))
+
+    player.gravity()
+
     if keysPressed[pg.K_LEFT]:
         player.x -= velocity
     elif keysPressed[pg.K_RIGHT]:
         player.x += velocity 
     elif keysPressed[pg.K_SPACE]:
         player.jump()
+
+    for event in pg.event.get():
+        if event.type == pg.KEYUP:
+            if event.key == pg.K_ESCAPE:
+                run = False
 
     pg.display.flip()
 
---
+++ neededCode.py
@@ -0,0 +1,317 @@
+def load(number, level):
+    import serverClass
+
+    levelServer = serverClass.Server('levelDB.db')
+
+    levelServer.executeQuery(f'SELECT BlockList FROM levels where ID = {number};')
+
+    blockList = levelServer.cursor.fetchall()[0][0]
+
+    blockList = blockList[2:-2].split('], [')
+
+    for listnum in range(len(blockList)):
+        blockList[listnum] = blockList[listnum].split(', ')
+
+        for num in range(len(blockList[listnum])):
+            blockList[listnum][num] = Tile(object = int(blockList[listnum][num]))
+
+    level.tileList = blockList
+
+class Level:
+
+    def __init__(self, name, xTiles, yTiles):
+
+        self.name = name
+
+        self.tileList = [[Tile() for _ in range(yTiles)] for _ in range(xTiles)]
+
+        self.targetList = []
+
+# I need to create a system where there is a grid of tiles
+# Each tile has its own actual tile that occupies its area
+class Tile:
+
+    def __init__(self, object = 0):
+        
+        # is an instance of a class
+        self.object = object
+
+class Air: # A empty tile
+
+    name = 'Air'
+
+    color = [255,255,255]
+
+    tileNum = 0
+
+    def playerInteraction(): # The player can move thorugh the block
+        return True
+
+    def laserInteraction(self, laser): # The laser can move through the block
+        pass
+
+class Block: # A block tile
+
+    name = 'Block'
+
+    color = [128,128,128]
+
+    tileNum = 1
+
+    def playerInteraction(): # The player cant move thorugh the block
+        return False
+
+    def laserInteraction(self, laser): # The laser cant move through the block
+        pass
+    
+class Glass: # A tile where light can go through but players cant
+
+    name = 'Glass'
+
+    color = [200,200,200]
+
+    tileNum = 2
+
+    def playerInteraction(): # The player cant move thorugh the block
+        return False
+
+    def laserInteraction(self, laser): # The laser can move through the block
+        pass
+
+class Smoke: # A tile where players can go through but light cant
+
+    name = 'Smoke'
+
+    color = [75,75,75]
+
+    tileNum = 3
+
+    def playerInteraction(): # The player can move thorugh the block
+        return True
+
+    def laserInteraction(self, laser): # The laser stops
+        pass
+
+class DoubleSidedMirror: # A tile that reflects lasers
+
+    name = 'DoubleSidedMirror'
+
+    color = [0,0,255]
+
+    tileNum = 4
+
+    def __init__(self):
+        self.direction = 0
+
+    def playerInteraction(): # The player cant move thorugh the block
+        return False
+
+    def laserInteraction(self, laser): # The laser changes direction by 90 degrees when interacting with this block
+        pass
+
+    def changeDirection(self):
+
+        if self.direction == 0:
+            self.direction = 1
+        elif self.direction == 1:
+            self.direction = 0
+        
+class OneSidedMirror: # A tile that reflects lasers
+
+    name = 'OneSidedMirror'
+
+    color = [0,0,128]
+
+    tileNum = 5
+
+    def __init__(self):
+        self.direction = 0
+
+    def playerInteraction(): # The player cant move thorugh the block
+        return False
+
+    def laserInteraction(self, laser): # The laser changes direction by 90 degrees when interacting with this block on the right side, ptherwise it gets stopped
+        pass
+
+    def changeDirection(self):
+
+        if self.direction == 0:
+            self.direction = 1
+        elif self.direction == 1:
+            self.direction = 2
+        elif self.direction == 2:
+            self.direction = 3
+        elif self.direction == 3:
+            self.direction = 0
+
+class Target: # A tile that unlocks the door
+
+    name = 'Target'
+
+    color = [255,0,0]
+
+    tileNum = 6
+
+    def __init__(self, level):
+        self.active = False
+
+        level.targetList.append(self)
+
+    def playerInteraction(): # The player can move thorugh the block
+        return True
+
+    def laserInteraction(self, laser): # The laser cant move thorugh the block, but it activates the target
+        pass
+
+class Lever: # A empty tile
+
+    name = 'Lever'
+
+    color = [165,42,42]
+
+    tileNum = 7
+
+    def __init__(self):
+
+        self.active = False
+
+        self.targetBlocks = []
+
+    def playerInteraction(): # The player can move thorugh the block
+        return True
+
+    def laserInteraction(self, laser): # The laser can move through the block
+        pass
+
+    def flipLever(self, level):
+        if self.active == True:
+            self.active = False
+
+            for num in range(len(self.targetBlocks)):
+                level.tileList[self.targetBlocks[num][1][0]][self.targetBlocks[num][1][1]].object = self.targetBlocks[num][0]
+                level.tileList[self.targetBlocks[num][3][0]][self.targetBlocks[num][3][1]].object = Air()
+
+        elif self.active == False:
+            self.active = True
+
+            for num in range(len(self.targetBlocks)):
+                level.tileList[self.targetBlocks[num][3][0]][self.targetBlocks[num][3][1]].object = self.targetBlocks[num][0]
+                level.tileList[self.targetBlocks[num][1][0]][self.targetBlocks[num][1][1]].object = self.targetBlocks[num][2]
+
+    def changeTargetBlock(self, level, block, startPos, endPos):
+        self.targetBlocks.append([block, startPos, level.tileList[endPos[0]][endPos[1]].object, endPos])
+
+    def deleteTargetBlock(self, block):
+        for num in range(len(self.targetBlocks)):
+            if self.targetBlocks[num][0] == block:
+                del self.targetBlocks[num]
+
+class LaserBeam:
+
+    name = 'LaserBeam'
+
+    color = [255,255,0]
+
+    tileNum = 8
+
+    def __init__(self):
+        self.direction = 0
+
+    def playerInteraction(): # The player cant move through a laser beam block
+        return False
+
+    def laserInteraciton(self, laser): # The laser wont go through the laser beam block
+        pass
+
+    def shootLaser(self): # shoots a laser in the direction it is facing
+        pass
+
+    def changeDirection(self):
+
+        if self.direction == 0:
+            self.direction = 1
+        elif self.direction == 1:
+            self.direction = 2
+        elif self.direction == 2:
+            self.direction = 3
+        elif self.direction == 3:
+            self.direction = 0
+
+class Entry:
+
+    name = 'Entry'
+
+    color = [0,128,0]
+
+    tileNum = 9
+
+    def playerInteraction(): # The player cant move through an entry way
+        return True
+
+    def laserInteraciton(self, laser): # The laser wont go through the entry
+        pass
+
+class Exit:
+
+    name = 'Exit'
+
+    color = [0,255,0]
+
+    tileNum = 10
+
+    def playerInteraction(self, level): # The player can only interact with an exit after all the target blocks have been activated
+        done = False
+
+        targetDone = 0
+
+        for target in level.targetList:
+            if target.active == True:
+                targetDone += 1
+
+        if targetDone == len(level.targetList):
+            return True
+        
+        else:
+            return False
+
+    def laserInteraciton(self, laser): # The laser wont go through the exit
+        pass
+
+class Portal:
+
+    name = 'Portal'
+
+    color = [255,0,255]
+
+    tileNum = 11
+
+    def __init___(self):
+
+        self.pairedPortal = None
+        self.pairedPortalPos = None
+
+        self.direction = 0
+
+    def playerInteraction(): # teleports player to the paired portal 
+        return True
+
+    def laserInteraction(self,laser): # teleports laser to the paired portal
+        pass
+
+    def pairPortal(self, portal, portalPos):
+        
+        self.pairedPortal = portal
+        self.pairedPortalPos = portalPos
+
+    def changeDirection(self):
+
+        if self.direction == 0:
+            self.direction = 1
+        elif self.direction == 1:
+            self.direction = 2
+        elif self.direction == 2:
+            self.direction = 3
+        elif self.direction == 3:
+            self.direction = 0
+
+tileList = [Air, Block, Glass, Smoke, DoubleSidedMirror, OneSidedMirror, Target, Lever, LaserBeam, Entry, Exit, Portal]
